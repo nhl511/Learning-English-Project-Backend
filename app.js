8d14cde7abc6e904b1e5b11aa5bda433a4fc3353
Nguyen Hoang Long <longnguyen5112000@gmail.com>
2025-02-07 14:33:35 +0700

    logout feat

--- app.js
+++ app.js
@@ -15,7 +15,10 @@ const authRouter = require("./routes/auth");
 const app = express();
 const cors = require('cors');
 
-app.use(cors())
+app.use(cors({
+    origin: process.env.FRONTEND_BASE_URL,
+    credentials: true,
+}))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
